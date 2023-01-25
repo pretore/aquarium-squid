e693f8df0fbce3524ccc5d536906b5ea7f7d8d05
Etienne Pretorius <ettienne_pretorius@yahoo.co.uk>
2023-01-25 01:37:25 +0000

    - added global ``squid_executor`` instance.

--- include/squid.h
+++ include/squid.h
@@ -9,4 +9,11 @@
 #include <squid/executor.h>
 #include <squid/future.h>
 
+/**
+ * @brief Retrieve global executor instance.
+ * @param [out] out receive global executor instance.
+ * @throws SQUID_EXECUTOR_ERROR_OUT_IS_NULL if out is <i>NULL</i>.
+ */
+bool squid_executor_instance(struct squid_executor **out);
+
 #endif /* _SQUID_SQUID_H_ */
--- src/squid.c
+++ src/squid.c
@@ -1,1 +1,43 @@
 #include <stdlib.h>
+#include <time.h>
+#include <errno.h>
+#include <seagrass.h>
+#include <triggerfish.h>
+#include <squid.h>
+
+static struct triggerfish_strong *executor_ref;
+static struct squid_executor *executor;
+
+bool squid_executor_instance(struct squid_executor **out) {
+    if (!out) {
+        squid_error = SQUID_EXECUTOR_ERROR_OUT_IS_NULL;
+        return false;
+    }
+    *out = executor;
+    return true;
+}
+
+__attribute__((constructor))
+static void on_load(void) {
+    seagrass_required_true(squid_executor_of(&executor_ref));
+    seagrass_required_true(triggerfish_strong_instance(
+            executor_ref, (void **) &executor));
+}
+
+__attribute__((destructor()))
+static void on_unload(void) {
+    seagrass_required_true(squid_executor_shutdown(executor));
+    uintmax_t count;
+    do {
+        seagrass_required_true(squid_executor_count(
+                executor, &count));
+        if (!count) {
+            break;
+        }
+        const struct timespec delay = {
+                .tv_nsec = 100000000 /* 100 milliseconds */
+        };
+        seagrass_required_true(!nanosleep(&delay, NULL)
+                               || errno == EINTR);
+    } while (true);
+}
