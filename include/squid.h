5c452273f8234c8b0f36ea4b3069ef527acb144b
Etienne Pretorius <ettienne_pretorius@yahoo.co.uk>
2023-01-26 00:16:10 +0000

    - Added methods to be used on_load or on_unload of the library to initialize or clean up internal library state.

--- include/squid.h
+++ include/squid.h
@@ -9,9 +9,23 @@
 #include <squid/executor.h>
 #include <squid/future.h>
 
+#define SQUID_ERROR_LIBRARY_UNINITIALIZED   \
+    (1 + SQUID_EXECUTOR_ERROR_OUT_IS_NULL)
+
+/**
+ * @brief Initialize library before use.
+ */
+void squid_on_load(void);
+
+/**
+ * @brief Clean up library variables before unloading.
+ */
+void squid_on_unload(void);
+
 /**
  * @brief Retrieve global executor instance.
  * @param [out] out receive global executor instance.
+ * @throws SQUID_ERROR_LIBRARY_UNINITIALIZED if library is uninitialized.
  * @throws SQUID_EXECUTOR_ERROR_OUT_IS_NULL if out is <i>NULL</i>.
  */
 bool squid_executor_instance(struct squid_executor **out);
--- src/squid.c
+++ src/squid.c
@@ -1,12 +1,18 @@
 #include <stdlib.h>
+#include <pthread.h>
 #include <seagrass.h>
 #include <triggerfish.h>
 #include <squid.h>
 
 static struct triggerfish_strong *executor_ref;
 static struct squid_executor *executor;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-bool squid_executor_instance(struct squid_executor **out) {
+bool squid_executor_instance(struct squid_executor **const out) {
+    if (!executor) {
+        squid_error = SQUID_ERROR_LIBRARY_UNINITIALIZED;
+        return false;
+    }
     if (!out) {
         squid_error = SQUID_EXECUTOR_ERROR_OUT_IS_NULL;
         return false;
@@ -16,13 +22,28 @@ bool squid_executor_instance(struct squid_executor **out) {
 }
 
 __attribute__((constructor))
-static void on_load(void) {
-    seagrass_required_true(squid_executor_of(&executor_ref));
-    seagrass_required_true(triggerfish_strong_instance(
-            executor_ref, (void **) &executor));
+void squid_on_load(void) {
+    if (executor_ref) {
+        return;
+    }
+    seagrass_required_true(!pthread_mutex_lock(&mutex));
+    if (!executor_ref) {
+        seagrass_required_true(squid_executor_of(&executor_ref));
+        seagrass_required_true(triggerfish_strong_instance(
+                executor_ref, (void **) &executor));
+    }
+    seagrass_required_true(!pthread_mutex_unlock(&mutex));
 }
 
 __attribute__((destructor))
-static void on_unload(void) {
-    seagrass_required_true(triggerfish_strong_release(executor_ref));
+void squid_on_unload(void) {
+    if (!executor_ref) {
+        return;
+    }
+    seagrass_required_true(!pthread_mutex_lock(&mutex));
+    if (executor_ref) {
+        seagrass_required_true(triggerfish_strong_release(executor_ref));
+        executor_ref = NULL;
+    }
+    seagrass_required_true(!pthread_mutex_unlock(&mutex));
 }
