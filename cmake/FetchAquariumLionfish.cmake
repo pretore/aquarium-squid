36d512784e11689ac707e7b48bb6159620703bbd
Etienne Pretorius <ettienne_pretorius@yahoo.co.uk>
2023-02-12 12:26:14 +0000

    - Updated dependency ``aquarium-lionfish`` to v1.0.2.

--- cmake/FetchAquariumLionfish.cmake
+++ cmake/FetchAquariumLionfish.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         aquarium-lionfish
         GIT_REPOSITORY https://github.com/pretore/aquarium-lionfish.git
-        GIT_TAG v1.0.0
+        GIT_TAG v1.0.2
         GIT_SHALLOW 1
 )
 
